4c9859ba27f0a3a47c68b9bbe40a595cd51f7242
Riley Tuttle <rileytuttle@Rileys-MacBook-Pro.local>
2018-11-11 09:28:48 -0500

    testing python module

--- thermostatFunctions.py
+++ thermostatFunctions.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python
 #thermostat on
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import Adafruit_DHT
-thermostatPin = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(thermostatPin, GPIO.OUT)
+# thermostatPin = 17
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(thermostatPin, GPIO.OUT)
 tempPin = 27
 sensor = 22
-class thermostat():
-	@staticmethod
-	def getTemp():
-		#probably more difficult than this
-		humidity, temperature = Adafruit_DHT(sensor, tempPin)
-		return temperature
-	@staticmethod
-	def setTemp(desiredTemp):
-		curTemp = getTemp()
-		if desiredTemp > curTemp:
-			GPIO.output(thermostatPin, GPIO.HIGH)
-		else:
-			GPIO.output(thermostatPin, GPIO.LOW)
-		return
-
-if __name__ == "__main__":
-    import sys
-    fib(int(sys.argv[1]))
+def getTemp():
+	humidity, temperature = Adafruit_DHT
+	return temperature
+# class thermostat():
+# 	@staticmethod
+# 	def getTemp():
+# 		#probably more difficult than this
+# 		humidity, temperature = Adafruit_DHT(sensor, tempPin)
+# 		return temperature
+# 	@staticmethod
+# 	def setTemp(desiredTemp):
+# 		curTemp = getTemp()
+# 		if desiredTemp > curTemp:
+# 			GPIO.output(thermostatPin, GPIO.HIGH)
+# 		else:
+# 			GPIO.output(thermostatPin, GPIO.LOW)
+# 		return
+#
+# if __name__ == "__main__":
+#     import sys
+#     fib(int(sys.argv[1]))
