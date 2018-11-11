474227b7e1da639729ae383c7a9b14c388a7da97
Riley <rileytuttle@gmail.com>
2018-11-11 14:13:22 +0000

    11/11/18

--- thermostatFunctions.py
+++ thermostatFunctions.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python
 #thermostat on
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+thermostatPin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(thermostatPin, GPIO.OUT)
+tempPin = 27
+sensor = 22
 class thermostat():
-	import RPi.GPIO as GPIO
-	import Adafruit_DHT
-	thermostatPin = 17
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(thermostatPin, GPIO.OUT)
-	tempPin = 27
-	sensor = 22
-
+	@staticmethod
 	def getTemp():
 		#probably more difficult than this
 		humidity, temperature = Adafruit_DHT(sensor, tempPin)
 		return temperature
+	@staticmethod
+	def setTemp(desiredTemp):
+		curTemp = getTemp()
+		if desiredTemp > curTemp:
+			GPIO.output(thermostatPin, GPIO.HIGH)
+		else:
+			GPIO.output(thermostatPin, GPIO.LOW)
+		return
 
-		def setTemp(desiredTemp):
-			curTemp = getTemp()
-			if desiredTemp > curTemp:
-				GPIO.output(thermostatPin, GPIO.HIGH)
-			else:
-				GPIO.output(thermostatPin, GPIO.LOW)
-				return
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
